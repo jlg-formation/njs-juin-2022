3b1d62cd580ffbe5745ef430342e30d97697cbd9
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-06-27 12:16:22 +0200

    dev: added hello world in express

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 console.log("About to start the server...");
+
+const express = require("express");
+const port = 3000;
+
+const app = express();
+
+app.use("/toto/titi", (req, res, next) => {
+  console.log("req: ", req.url);
+  res.json({ toto: "titi" });
+});
+
+app.listen(port, () => {
+  console.log("Server started with success on port " + port);
+});
