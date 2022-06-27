fe5f3048ef4243b0b1d5e02d1e2be5f8e1d1f52d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-06-27 14:19:07 +0200

    dev: express static server

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 console.log("About to start the server...");
 
 const express = require("express");
+const serveIndex = require("serve-index");
 const port = 3000;
 
 const app = express();
 
-app.use("/toto/titi", (req, res, next) => {
+app.use((req, res, next) => {
   console.log("req: ", req.url);
+  next();
+});
+
+app.use("/toto/titi", (req, res, next) => {
   res.json({ toto: "titi" });
 });
+
+app.use(express.static("."));
+app.use(serveIndex("."));
 
 app.listen(port, () => {
   console.log("Server started with success on port " + port);
