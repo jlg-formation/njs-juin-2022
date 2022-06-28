a598b6003820df611e44de59cb28df3f8fce714e
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-06-28 15:22:33 +0200

    dev: refactored server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,12 @@
+import { WebServer } from "./WebServer";
+
 console.log("About to start the server...");
 
-import express from "express";
-import serveIndex from "serve-index";
-
-import { api } from "./api";
-
-const port = 3000;
-const app = express();
-
-app.use((req, res, next) => {
-  console.log("req: ", req.url);
-  next();
-});
-
-app.use("/toto/titi", (req, res, next) => {
-  res.json({ toto: "titi" });
-});
-
-app.use("/api", api);
-
-app.use(express.static("."));
-app.use(serveIndex(".", { icons: true }));
-
-app.listen(port, () => {
-  console.log(`Server started with success on port ${port}`);
-});
+(async () => {
+  try {
+    const server = new WebServer();
+    await server.start();
+  } catch (err) {
+    console.log("err: ", err);
+  }
+})();
