c32d5cf99c01fcc39e53bf892b48de3e02719db9
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-06-29 15:10:16 +0200

    dev: added test poc with sequelize

--- sandbox/sequelize/test.js
+++ sandbox/sequelize/test.js
@@ -1,4 +1,4 @@
-const { Sequelize } = require("sequelize");
+const { Sequelize, DataTypes } = require("sequelize");
 
 const sequelize = new Sequelize("mariadb://root:admin@localhost:3306/njs");
 
@@ -6,6 +6,30 @@ const sequelize = new Sequelize("mariadb://root:admin@localhost:3306/njs");
   try {
     await sequelize.authenticate();
     console.log("Connection has been established successfully.");
+
+    const User = sequelize.define(
+      "User",
+      {
+        // Model attributes are defined here
+        firstName: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        lastName: {
+          type: DataTypes.STRING,
+          // allowNull defaults to true
+        },
+      },
+      {
+        // Other model options go here
+      }
+    );
+
+    await sequelize.sync({ force: true });
+
+    const jane = await User.create({ firstName: "Jane", lastName: "Doe" });
+    console.log("Jane's auto-generated ID:", jane.id);
+
     await sequelize.close();
   } catch (error) {
     console.error("Unable to connect to the database:", error);
