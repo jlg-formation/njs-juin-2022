5f8b6ec4a82cf4759928ee829c7c50fce9853576
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-06-29 17:35:36 +0200

    dev: timeout mocha

--- test/webserver.spec.ts
+++ test/webserver.spec.ts
@@ -1,14 +1,18 @@
 import assert from "assert";
 import axios from "axios";
+import { sleep } from "../src/misc";
 import { WebServer } from "../src/WebServer";
 
-describe("WebServer", function () {
-  it("should start and stop correctly", async () => {
+describe.only("WebServer", () => {
+  it("should start and stop correctly", async function () {
+    this.timeout(5000);
     const webServer = new WebServer({ port: 3456 });
     await webServer.start();
     // test that it is well started by doing an HTTP request on it.
     const response = await axios.get("http://localhost:3456/api/ping");
     assert(response.status === 200);
+
+    await sleep(2500);
     await webServer.stop();
     // test that it is well stopped by doing an HTTP request on it.
     let error = undefined;
