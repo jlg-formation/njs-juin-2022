ac402e565f8efc3441c8030d3ca05159bc461f39
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-06-29 15:36:59 +0200

    dev: poc sequelize in ts

--- sandbox/sequelize/test.ts
+++ sandbox/sequelize/test.ts
@@ -1,31 +1,38 @@
+import { Model } from "sequelize";
+
 const { Sequelize, DataTypes } = require("sequelize");
 
 const sequelize = new Sequelize("mariadb://root:admin@localhost:3306/njs");
+
+class User extends Model {
+  declare id: number;
+  declare firstName: string;
+  declare lastName: string;
+}
+
+User.init(
+  {
+    // Model attributes are defined here
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      // allowNull defaults to true
+    },
+  },
+  {
+    sequelize, // passing the `sequelize` instance is required
+  }
+);
 
 (async () => {
   try {
     await sequelize.authenticate();
     console.log("Connection has been established successfully.");
 
-    const User = sequelize.define(
-      "User",
-      {
-        // Model attributes are defined here
-        firstName: {
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
-        lastName: {
-          type: DataTypes.STRING,
-          // allowNull defaults to true
-        },
-      },
-      {
-        // Other model options go here
-      }
-    );
-
-    await sequelize.sync({ force: true });
+    await sequelize.sync({ force: false });
 
     const jane = await User.create({ firstName: "Jane", lastName: "Doe" });
     console.log("Jane's auto-generated ID:", jane.id);
