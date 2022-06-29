fec45aa02dad3680d0ca2b188938a8e9e600570d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-06-29 17:01:39 +0200

    dev: 6 test passed with sequelize

--- src/services/SequelizeService.ts
+++ src/services/SequelizeService.ts
@@ -27,7 +27,9 @@ export class SequelizeService extends AbstractService {
       await this.sequelize.sync({ force: false });
       this.server.once("close", () => {
         (async () => {
+          console.log("closing sequelize...");
           await this.sequelize.close();
+          console.log("sequelize closed.");
         })();
       });
       this.eventEmmitter.emit("isReady");
@@ -37,31 +39,69 @@ export class SequelizeService extends AbstractService {
   }
 
   async createOneArticle(newArticle: NewArticle): Promise<{ id: string }> {
-    console.log("create one article");
-    const result = await ArticleModel.create(newArticle);
-    console.log("result: ", result);
-    const id = result.id;
-    return { id: String(id) };
+    try {
+      console.log("create one article");
+      const result = await ArticleModel.create(newArticle);
+      console.log("result: ", result);
+      const id = result.id;
+      return { id: String(id) };
+    } catch (err) {
+      console.log("err: ", err);
+      throw err;
+    }
   }
 
-  deleteAllArticle(): Promise<void> {
-    throw new Error("Method not implemented.");
+  async deleteAllArticle(): Promise<void> {
+    try {
+      await ArticleModel.destroy({
+        where: {},
+        truncate: false,
+      });
+    } catch (err) {
+      console.log("err: ", err);
+      throw err;
+    }
   }
-  deleteOneArticle(id: string): Promise<void> {
-    throw new Error("Method not implemented.");
+  async deleteOneArticle(id: string): Promise<void> {
+    try {
+      await ArticleModel.destroy({
+        where: { id: id },
+      });
+    } catch (err) {
+      console.log("err: ", err);
+      throw err;
+    }
   }
-  retrieveAllArticle(): Promise<Article[]> {
-    throw new Error("Method not implemented.");
+  async retrieveAllArticle(): Promise<Article[]> {
+    try {
+      const result = await ArticleModel.findAll({
+        where: {},
+      });
+      const articles = result.map((am) => {
+        const json = am.toJSON();
+        const article = { ...json, id: String(json.id) };
+        return article;
+      });
+      return articles;
+    } catch (err) {
+      console.log("err: ", err);
+      throw err;
+    }
   }
   async retrieveOneArticle(id: string): Promise<Article | undefined> {
-    const result = await ArticleModel.findByPk(id);
-    console.log("result: ", result);
-    if (result === null) {
-      return undefined;
+    try {
+      const result = await ArticleModel.findByPk(id);
+      console.log("result: ", result);
+      if (result === null) {
+        return undefined;
+      }
+      const json = result.toJSON();
+      const article = { ...json, id: String(json.id) };
+      console.log("article: ", article);
+      return article as Article;
+    } catch (err) {
+      console.log("err: ", err);
+      throw err;
     }
-    const json = result.toJSON();
-    const article = { ...json, id: String(json.id) };
-    console.log("article: ", article);
-    return article as Article;
   }
 }
